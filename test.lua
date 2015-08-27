5cca48088f00e75c5ff9983dc805ae112de93748
Noé Falzon <falzon.noe@gmail.com>
2015-08-27 13:58:38 +0300

    Computed web pages

--- test.lua
+++ test.lua
@@ -1,9 +1,12 @@
-require "tree"
-require "graph"
-require "free"
+require "make_html"
 
-res = make_all_free_trees(8)
+fp = io.open("1-12.html", "w")
+make_html(fp,1,12)
+fp:close()
 
-for i,t in ipairs(res) do
-	print(t.namestring)
+for i = 13,15 do
+	print("Treating " .. i)
+	fp = io.open(i .. ".html", "w")
+	make_html(fp, i)
+	fp:close()
 end
